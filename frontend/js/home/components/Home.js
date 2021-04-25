77bb5c4943fdada71f90a77cd809152caab518d2
Kresna <satyakresna6295@gmail.com>
2021-04-25 10:08:48 +0800

    rename bfwiki to XBraveFrontier

--- frontend/js/home/components/Home.js
+++ frontend/js/home/components/Home.js
@@ -5,12 +5,12 @@ export default function () {
         <h1 class="text-3xl my-4"><strong>Brave Frontier Wiki (Unofficial)</strong></h1>
         <div class="leading-loose">
           <p>This website provides list of omni units and Dual Brave Burst (DBB) from Brave Frontier Global.</p>
-          <p>The data take from <a href="https://github.com/satyakresna/bravefrontier" target="_blank" rel="noopener" class="underline text-blue-700">unofficial Brave Frontier API that created by myself</a>.</p>
-          <p>The source code of this website is available by visit to <a href="https://github.com/satyakresna/bfwiki" target="_blank" rel="noopener" class="underline text-blue-700">github.com/satyakresna/bfwiki</a>.</p>
+          <p>The data take from <a href="https://bravefrontierglobal.fandom.com/wiki/Brave_Frontier_Wiki" target="_blank" rel="noopener" class="underline text-blue-700">Brave Frontier Global Fandom site</a>.</p>
+          <p>The source code of this website is available by visit to <a href="https://github.com/satyakresna/xbravefrontier" target="_blank" rel="noopener" class="underline text-blue-700">github.com/satyakresna/xbravefrontier</a>.</p>
         </div>
         <h2 class="text-2xl my-4"><strong>Feedback</strong></h2>
         <div class="leading-loose">
-          <p>If you have any feedback about feature(s) on this website please submit form in <a href="https://github.com/satyakresna/bfwiki/issues" target="_blank" rel="noopener" class="underline text-blue-700">github.com/satyakresna/bfwiki/issues</a></p>
+          <p>If you have any feedback about feature(s) on this website please submit form in <a href="https://github.com/satyakresna/xbravefrontier/issues" target="_blank" rel="noopener" class="underline text-blue-700">github.com/satyakresna/xbravefrontier/issues</a></p>
         </div>
         <h2 class="text-2xl my-4"><strong>Join me</strong></h2>
         <div class="leading-loose">
