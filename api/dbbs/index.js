370b53434f6a179cc6f2f37af82ed53d4573e1f8
Kresna Satya <satyakresna6295@gmail.com>
2022-07-11 20:24:27 +0800

    allow cors

--- api/dbbs/index.js
+++ api/dbbs/index.js
@@ -39,6 +39,11 @@ exports.handler = async (event, callback) => {
 
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        },
         body: JSON.stringify(result)
     };
 }
--- api/omniunits/index.js
+++ api/omniunits/index.js
@@ -82,6 +82,11 @@ exports.handler = async (event, context) => {
 
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        },
         body: JSON.stringify(result)
     };
 }
